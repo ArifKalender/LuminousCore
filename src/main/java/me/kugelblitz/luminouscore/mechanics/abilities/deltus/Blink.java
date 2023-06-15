a9f089db9e3cc93f2772f65e1bc3bd1647f52ee7
Arif Kalender <72840955+SolidSteed@users.noreply.github.com>
2023-06-15 17:03:36 +0300

    0.0.11-ALPHA update

---
+++ src/main/java/me/kugelblitz/luminouscore/mechanics/abilities/deltus/Blink.java
@@ -0,0 +1,50 @@
+package me.kugelblitz.luminouscore.mechanics.abilities.deltus;
+
+import me.kugelblitz.luminouscore.LuminousCore;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+public class Blink {
+
+    int i=0;
+    public Blink(Player player){
+
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                if(i<1){
+                    i++;
+                    Location location = player.getEyeLocation();
+                    Vector direction = location.getDirection();
+
+                    for (int i = 0; i <= 20; i++) {
+                        location.add(direction);
+
+                        if (location.getBlock().getType() == Material.AIR || location.getBlock().getType() == Material.WATER) {
+                            location.getWorld().spawnParticle(Particle.WATER_SPLASH, location, 5, 0.2, 0.2, 0.2, 0.05);
+                        } else {
+                            location.add(direction.multiply(-1));
+                            break;
+                        }
+                    }
+
+                    player.teleport(location);
+                    player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 4, 0f);
+                }else {
+                    i=0;
+                    player.setVelocity(player.getLocation().getDirection().multiply(1));
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(LuminousCore.plugin,0,1);
+
+
+
+    }
+
+}
