2fa49f18d02fcc4e60ab6f473c222f13b349828f
Arif Kalender <72840955+SolidSteed@users.noreply.github.com>
2023-06-17 23:33:43 +0300

    0.0.12.1-ALPHA update

---
+++ src/main/java/me/kugelblitz/luminouscore/mechanics/PermaParties/Party.java
@@ -0,0 +1,14 @@
+package me.kugelblitz.luminouscore.mechanics.PermaParties;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+//party command
+public class Party implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+}
